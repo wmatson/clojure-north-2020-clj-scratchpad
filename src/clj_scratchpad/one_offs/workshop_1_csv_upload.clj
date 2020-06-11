92e05a4b80dc588594419f009ac77142a94a9c9d
wmatson <wmatson@live.com>
2020-06-11 13:40:44 -0600

    Completed code for workshop-exercise-1

--- src/clj_scratchpad/one_offs/workshop_1_csv_upload.clj
+++ src/clj_scratchpad/one_offs/workshop_1_csv_upload.clj
@@ -4,7 +4,20 @@
             [clojure.java.io :as io]
             [medley.core :as medley]))
 
+(defn- keywordify-keys [event]
+  (medley/map-keys keyword event))
+
 (comment
   ;; Upload location: http://workshop.wmatson.com:8080/
 
-  (take 10 (csvu/read-maps "resources/exercise-1.csv")))
+  (->> (csvu/read-maps "resources/exercise-1.csv")
+       (map keywordify-keys)
+       (group-by :event-id)
+       (medley/filter-vals #(> (count %) 1))
+       (take 10))
+  
+  (with-open [writer (io/writer "resources/exercise-1-corrected.csv")]
+    (->> (csvu/read-maps "resources/exercise-1.csv")
+         set
+         (csvu/maps->csv-data ["event-id" "event-name" "event-type" "extra-1"])
+         (csv/write-csv writer))))
