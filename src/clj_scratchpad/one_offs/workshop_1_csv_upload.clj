18e56ff35ce07cf8891a8b1df44813c7e819a250
wmatson <wmatson@live.com>
2020-06-11 13:29:09 -0600

    Changing up scaffolding imports for exercise-1

--- src/clj_scratchpad/one_offs/workshop_1_csv_upload.clj
+++ src/clj_scratchpad/one_offs/workshop_1_csv_upload.clj
@@ -1,5 +1,7 @@
 (ns clj-scratchpad.one-offs.workshop-1-csv-upload
-  (:require [clj-scratchpad.utils.csv :as csv]
+  (:require [clj-scratchpad.utils.csv :as csvu]
+            [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
             [medley.core :as medley]))
 
 (comment
